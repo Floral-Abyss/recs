ac5e3189632dde44f17b3fe36423ccc075af53c4
Lily Brown <git@amaranthinecodices.me>
2019-03-18 20:36:44 -0700

    component property overriding
    allows you to use a module script named ComponentProps in an instance to override component properties
    
    you can only override component props, not add new ones; an error will be thrown otherwise
    
    there are also better error messages in defineComponent
    
    closes #1

--- src/defineComponent.lua
+++ src/defineComponent.lua
@@ -4,8 +4,23 @@ todo lol
 
 ]]
 
+local errorFormats = {
+    nonStringName = "tagName (1) must be a string, is a %s",
+    nonTableDefaultProps = "defaultProps (2) must be a table or nil, is a %s",
+    nonModuleScriptPropOverride = "Component property override module %s must be a ModuleScript, is a %s",
+    nonTableRootPropOverride = "Component property override module %s must return a table, returned a %s",
+    nonTableComponentPropOverride = "Component property override entry %s from module %s must return a table, returned a %s",
+    overrideNonexistentKey = "Component property override module %s is trying to override %s.%s, which does not exist in the %s component.",
+}
+
 function defineComponent(tagName, defaultProps)
-    assert(typeof(tagName) == "string", "tagName (1) must be a string, is a " .. typeof(tagName))
+    assert(
+        typeof(tagName) == "string",
+        errorFormats.nonStringName(typeof(tagName)))
+    
+    assert(
+        defaultProps == nil or typeof(defaultProps) == "table",
+        errorFormats.nonTableDefaultProps:format(typeof(defaultProps)))
 
     local definition = {}
     definition.tagName = tagName
@@ -20,6 +35,34 @@ function defineComponent(tagName, defaultProps)
 
         component.tagName = tagName
         component.instance = instance
+
+        local componentPropsModule = instance:FindFirstChild("ComponentProps")
+        if componentPropsModule ~= nil then
+            local fullName = componentPropsModule:GetFullName()
+            assert(
+                componentPropsModule:IsA("ModuleScript"),
+                errorFormats.nonModuleScriptPropOverride:format(fullName, componentPropsModule.ClassName))
+
+            local componentProps = require(componentPropsModule)
+            assert(
+                typeof(componentProps) == "table",
+                errorFormats.nonTableRootPropOverride:format(fullName, typeof(componentProps)))
+
+            local specificComponentProps = componentProps[tagName]
+            assert(
+                specificComponentProps == nil or typeof(specificComponentProps) == "table",
+                errorFormats.nonTableComponentPropOverride:format(tagName, fullName, typeof(specificComponentProps)))
+
+            if specificComponentProps ~= nil then
+                for overrideKey, overrideValue in pairs(specificComponentProps) do
+                    assert(
+                        definition.defaultProps[overrideKey] ~= nil,
+                        errorFormats.overrideNonexistentKey:format(fullName, tagName, overrideKey, tagName))
+                    
+                    component[overrideKey] = overrideValue
+                end
+            end
+        end
 
         if definition.init then
             definition.init(component, instance)
