0d51cd32da3cf031866bcc4f32cb338c0912ea00
AmaranthineCodices <git@amaranthinecodices.me>
2019-04-19 00:28:50 -0700

    module doc for defineRegistration

--- src/defineRegistration.lua
+++ src/defineRegistration.lua
@@ -1,3 +1,28 @@
+--[[
+
+    defineRegistration provides constructors for stepper definitions, which are
+    instructions that Cores can use to create a stepper object to step systems
+    in a defined order.
+
+    The stepper definitions contain the kind of stepper to be created, any
+    parameters that the stepper needs, and an array of system classes. When the
+    Core creates a stepper, it maps the system classes to system instances that
+    were created when the system classes were registered with the Core.
+
+    Each stepper definition must have two fields:
+    - type (string): The type of stepper that this definition refers to. Current
+      valid values are "interval" and "event".
+    - systemClasses (array): An array of system classes that the stepper will
+      step when it is created.
+
+    Any other fields are determined by the value of the type field.
+
+    Cores will throw an error if they don't understand the value of the type
+    field. Since stepper definitions are created using functions defined in this
+    file, this shouldn't be encountered in normal use cases.
+
+]]
+
 local System = require(script.Parent.System)
 
 local errorMessages = {
